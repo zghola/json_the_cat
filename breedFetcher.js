1893bd31504fb1eb7caecbda14adc652af5c87fa
zinat <zinat.gholami@gmail.com>
2021-11-04 21:27:44 +0000

    first commit

---
+++ breedFetcher.js
@@ -0,0 +1,39 @@
+const request = require('request');
+
+const fetchBreedDescription = function(breedName, callback) {
+
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, resp, body) => {
+    console.log(body);
+    console.log(typeof body);
+
+    if (error) {
+      callback(`Failed to request details: ${error}`,null);
+    }
+
+    const data = JSON.parse(body);
+    console.log(data);
+
+    const breed = data[0];
+    if (breed) {
+      callback(null,breed.description);
+    } else {
+      callback(`Failed to find breed ${breedName}`,null);
+    }
+  });
+
+};
+const breedName = process.argv[2];
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(desc);
+  }
+});
+
+
+
+
+  
