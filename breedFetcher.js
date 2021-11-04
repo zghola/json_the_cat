6a9932bb0355265ac3dac2cba9271cac0475ee1e
zinat <zinat.gholami@gmail.com>
2021-11-04 22:10:52 +0000

    Testing Cats as a Service

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,18 @@
 const request = require('request');
 
 
-const breedFetcher = function(breedName, callback) {
+const fetchBreedDescription = function(breedName, callback) {
 
   const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
   request(url, (error, resp, body) => {
-    console.log(body);
-    console.log(typeof body);
+    
 
     if (error) {
       callback(`Failed to request details: ${error}`,null);
     }
 
     const data = JSON.parse(body);
-    console.log(data);
+    
 
     const breed = data[0];
     if (breed) {
@@ -24,15 +23,10 @@ const breedFetcher = function(breedName, callback) {
   });
 
 };
-const breedName = process.argv[2];
 
-breedFetcher(breedName, (error, desc) => {
-  if (error) {
-    console.log('Error fetch details:', error);
-  } else {
-    console.log(desc);
-  }
-});
+
+
+module.exports = { fetchBreedDescription };
 
 
 
